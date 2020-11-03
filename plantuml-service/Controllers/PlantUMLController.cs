a8330022cbc45f07606831734a32a7e42a87fe7b
Johannes Poehn <johannes.poehn@gmail.com>
2020-11-03 02:36:06 +0100

    PlantUML Controller

---
+++ plantuml-service/Controllers/PlantUMLController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PlantUml.Net;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace plantuml_service.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PlantUMLController : ControllerBase
+    {
+        private readonly ILogger<PlantUMLController> _logger;
+
+        public PlantUMLController(ILogger<PlantUMLController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET api/<PlantUMLController>/5
+        [HttpGet]
+        public async Task<HttpResponseMessage> Get([FromForm] string code)
+        {
+            var factory = new RendererFactory();
+            if (code is null)
+            {
+                return new HttpResponseMessage(HttpStatusCode.NoContent);
+            }
+            var renderer = factory.CreateRenderer(new PlantUmlSettings());
+            var bytes = await renderer.RenderAsync(code, OutputFormat.Png); //"Bob -> Alice : Hello"
+            var result = new HttpResponseMessage(HttpStatusCode.OK);
+            result.Content = new ByteArrayContent(bytes);
+            result.Content.Headers.ContentType = new MediaTypeHeaderValue("image/png");
+            return result;
+        }
+    }
+}
